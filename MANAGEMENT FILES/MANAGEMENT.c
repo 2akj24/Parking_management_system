c9d36ce258c9aa46239eeb7403a23d388507488e
Abhinav kumar Jha <ak80001j@gmail.com>
2025-06-19 00:59:35 +0530

    Initial Update

--- MANAGEMENT FILES/MANAGEMENT.c
+++ MANAGEMENT FILES/MANAGEMENT.c
@@ -10,8 +10,8 @@
 struct Vehicle {
     char licensePlate[20];
     char ownerName[MAX_NAME_LENGTH];
-    int type; // 2 for 2-wheelers, 4 for 4-wheelers
-    char parkingDate[11]; // Format: YYYY-MM-DD
+    int type;
+    char parkingDate[11];
 };
 
 // Function prototypes
@@ -20,16 +20,16 @@ void parkVehicle(struct Vehicle parkingLot[], int* totalVehicles, MYSQL* conn);
 void removeVehicle(struct Vehicle parkingLot[], int* totalVehicles, MYSQL* conn);
 void viewParkedVehicles(struct Vehicle parkingLot[], int totalVehicles, MYSQL* conn);
 void searchHistorySQL(MYSQL *conn);
-void generateInvoice(struct Vehicle vehicle, char departureDate[11]);
 void displayHeader();
 void displayFooter();
+int convertTo24Hour(const char* timeStr);
+float calculateFare(const char* startDate, const char* startTime, const char* endDate, const char* endTime, int type);
 
 int main() {
     struct Vehicle parkingLot[MAX_VEHICLES];
     int totalVehicles = 0;
     int choice;
 
-    // MySQL Connection
     MYSQL *conn;
     conn = mysql_init(NULL);
     if (mysql_real_connect(conn, "localhost", "root", "1234", "ParkingDB", 0, NULL, 0) == NULL) {
@@ -62,8 +62,6 @@ int main() {
     return 0;
 }
 
-// ========== MENU & UI ==========
-
 void displayMenu() {
     printf("\nShivaay Parking Management System\n");
     printf("1. Park a vehicle\n");
@@ -74,32 +72,30 @@ void displayMenu() {
 }
 
 void displayHeader() {
-    printf("****************************************************\n");
-    printf("**            Shivaay Parking Management           **\n");
-    printf("****************************************************\n");
-    printf("  \n");
-    printf("**                   Created by                    **\n");
-    printf("**                 Byte Grinders                   **\n");
-    printf("****************************************************\n");
+    printf("******************************************************\n");
+    printf("**            Shivaay Parking Management            **\n");
+    printf("******************************************************\n");
+    printf("** Fare: 2 Wheeler - Rs 50/hr 4 Wheeler- Rs 200/hr   **\n");
+    printf("*******************************************************\n");
 }
 
 void displayFooter() {
     printf("****************************************************\n");
     printf("** Thanks for using Shivaay Parking Management!   **\n");
     printf("**        Hope you like the service               **\n");
-    printf("**         Created by Byte Grinders               **\n");
+    printf("**            Created by Abhinav                  **\n");
     printf("****************************************************\n");
 }
 
-// ========== PARK VEHICLE ==========
-
 void parkVehicle(struct Vehicle parkingLot[], int* totalVehicles, MYSQL* conn) {
     if (*totalVehicles < MAX_VEHICLES) {
+        char checkinTime[10];
+
         printf("Enter vehicle license plate: ");
-        scanf(" %[^\n]s", parkingLot[*totalVehicles].licensePlate);
+        scanf(" %[^\n]", parkingLot[*totalVehicles].licensePlate);
 
         printf("Enter vehicle owner's name: ");
-        scanf(" %[^\n]s", parkingLot[*totalVehicles].ownerName);
+        scanf(" %[^\n]", parkingLot[*totalVehicles].ownerName);
 
         printf("Enter vehicle type (2 for 2-wheelers, 4 for 4-wheelers): ");
         scanf("%d", &parkingLot[*totalVehicles].type);
@@ -107,14 +103,18 @@ void parkVehicle(struct Vehicle parkingLot[], int* totalVehicles, MYSQL* conn) {
         printf("Enter parking date (YYYY-MM-DD): ");
         scanf("%s", parkingLot[*totalVehicles].parkingDate);
 
-        // Insert into MySQL active table
+        printf("Enter check-in time (e.g., 9AM): ");
+        scanf("%s", checkinTime);
+
         char query[512];
         sprintf(query,
-            "INSERT INTO active (license_plate, owner_name, type, parking_date) VALUES ('%s', '%s', %d, '%s')",
+            "INSERT INTO active (license_plate, owner_name, type, parking_date, checkin_time) "
+            "VALUES ('%s', '%s', %d, '%s', '%s')",
             parkingLot[*totalVehicles].licensePlate,
             parkingLot[*totalVehicles].ownerName,
             parkingLot[*totalVehicles].type,
-            parkingLot[*totalVehicles].parkingDate);
+            parkingLot[*totalVehicles].parkingDate,
+            checkinTime);
 
         if (mysql_query(conn, query) != 0) {
             fprintf(stderr, "Error inserting into database: %s\n", mysql_error(conn));
@@ -127,18 +127,17 @@ void parkVehicle(struct Vehicle parkingLot[], int* totalVehicles, MYSQL* conn) {
     }
 }
 
-// ========== REMOVE VEHICLE ==========
-
 void removeVehicle(struct Vehicle parkingLot[], int* totalVehicles, MYSQL* conn) {
-    char licensePlate[20];
+    char licensePlate[20], departureDate[11], departureTime[10];
     printf("Enter vehicle license plate: ");
-    scanf(" %[^\n]s", licensePlate);
+    scanf(" %[^\n]", licensePlate);
 
-    char departureDate[11];
     printf("Enter departure date (YYYY-MM-DD): ");
     scanf("%s", departureDate);
 
-    // Fetch from `active`
+    printf("Enter departure time (e.g., 11AM): ");
+    scanf("%s", departureTime);
+
     char selectQuery[256];
     sprintf(selectQuery, "SELECT * FROM active WHERE license_plate = '%s'", licensePlate);
 
@@ -147,43 +146,28 @@ void removeVehicle(struct Vehicle parkingLot[], int* totalVehicles, MYSQL* conn)
         MYSQL_ROW row = mysql_fetch_row(res);
 
         if (row) {
-            // Insert into history
-            char insertQuery[512];
+            float totalCharge = calculateFare(row[3], row[4], departureDate, departureTime, atoi(row[2]));
+
+            char insertQuery[1024];
             sprintf(insertQuery,
-                "INSERT INTO history (license_plate, owner_name, type, parking_date, departure_date) "
-                "VALUES ('%s', '%s', %d, '%s', '%s')",
-                row[0], row[1], atoi(row[2]), row[3], departureDate);
+                "INSERT INTO history (license_plate, owner_name, type, parking_date, departure_date, checkin_time, checkout_time, fare) "
+                "VALUES ('%s', '%s', %d, '%s', '%s', '%s', '%s', %.2f)",
+                row[0], row[1], atoi(row[2]), row[3], departureDate, row[4], departureTime, totalCharge);
             mysql_query(conn, insertQuery);
 
-            // Delete from active
             char deleteQuery[256];
             sprintf(deleteQuery, "DELETE FROM active WHERE license_plate = '%s'", licensePlate);
             mysql_query(conn, deleteQuery);
 
-            // Also remove from in-memory list
-            for (int i = 0; i < *totalVehicles; i++) {
-                if (strcmp(parkingLot[i].licensePlate, licensePlate) == 0) {
-                    generateInvoice(parkingLot[i], departureDate);
-                    for (int j = i; j < *totalVehicles - 1; j++) {
-                        parkingLot[j] = parkingLot[j + 1];
-                    }
-                    (*totalVehicles)--;
-                    break;
-                }
-            }
-
-            printf("Vehicle removed and moved to history.\n");
+            printf("Vehicle removed and moved to history. Total Fare: Rs%.2f\n", totalCharge);
         } else {
             printf("Vehicle not found in database.\n");
         }
-
         mysql_free_result(res);
     } else {
         fprintf(stderr, "Query error: %s\n", mysql_error(conn));
     }
 }
-
-// ========== VIEW PARKED VEHICLES ==========
 
 void viewParkedVehicles(struct Vehicle parkingLot[], int totalVehicles, MYSQL* conn) {
     if (mysql_query(conn, "SELECT * FROM active") == 0) {
@@ -193,13 +177,10 @@ void viewParkedVehicles(struct Vehicle parkingLot[], int totalVehicles, MYSQL* c
         printf("\n****************************************************\n");
         printf("            Currently Parked Vehicles\n");
         printf("****************************************************\n");
-        printf("License Plate\tOwner's Name\tType\tParking Date\n");
+        printf("License Plate\tOwner\tType\tDate\tTime\n");
 
         while ((row = mysql_fetch_row(res))) {
-            printf("%s\t\t%s\t\t%s\t%s\n",
-                   row[0], row[1],
-                   (atoi(row[2]) == 2) ? "2-wheeler" : "4-wheeler",
-                   row[3]);
+            printf("%s\t%s\t%s\t%s\t%s\n", row[0], row[1], row[2], row[3], row[4]);
         }
 
         mysql_free_result(res);
@@ -207,8 +188,6 @@ void viewParkedVehicles(struct Vehicle parkingLot[], int totalVehicles, MYSQL* c
         fprintf(stderr, "Error fetching from active table: %s\n", mysql_error(conn));
     }
 }
-
-// ========== SEARCH HISTORY ==========
 
 void searchHistorySQL(MYSQL *conn) {
     int option;
@@ -219,7 +198,7 @@ void searchHistorySQL(MYSQL *conn) {
 
     if (option == 1) {
         printf("Enter license plate: ");
-        scanf(" %[^\n]s", input);
+        scanf(" %[^\n]", input);
         sprintf(query, "SELECT * FROM history WHERE license_plate = '%s'", input);
     } else if (option == 2) {
         printf("Enter parking date (YYYY-MM-DD): ");
@@ -237,10 +216,11 @@ void searchHistorySQL(MYSQL *conn) {
         printf("\n****************************************************\n");
         printf("                Search Results\n");
         printf("****************************************************\n");
-        printf("License\tOwner\tType\tParked\tDeparted\n");
+        printf("License\tOwner\tType\tIn\tOut\tFare\n");
 
         while ((row = mysql_fetch_row(res))) {
-            printf("%s\t%s\t%s\t%s\t%s\n", row[0], row[1], row[2], row[3], row[4]);
+            printf("%s\t%s\t%s\t%s %s\t%s %s\tRs%.2f\n",
+                   row[0], row[1], row[2], row[3], row[5], row[4], row[6], atof(row[7]));
         }
 
         mysql_free_result(res);
@@ -249,32 +229,29 @@ void searchHistorySQL(MYSQL *conn) {
     }
 }
 
-// ========== GENERATE INVOICE ==========
+int convertTo24Hour(const char* timeStr) {
+    int hour;
+    char ampm[3];
+    sscanf(timeStr, "%d%2s", &hour, ampm);
+    if ((strcmp(ampm, "PM") == 0 || strcmp(ampm, "pm") == 0) && hour != 12)
+        hour += 12;
+    if ((strcmp(ampm, "AM") == 0 || strcmp(ampm, "am") == 0) && hour == 12)
+        hour = 0;
+    return hour;
+}
 
-void generateInvoice(struct Vehicle vehicle, char departureDate[11]) {
-    const float RATE_2_WHEELER = 450.0;
-    const float RATE_4_WHEELER = 650.0;
+float calculateFare(const char* startDate, const char* startTime, const char* endDate, const char* endTime, int type) {
+    int sYear, sMonth, sDay, eYear, eMonth, eDay;
+    sscanf(startDate, "%d-%d-%d", &sYear, &sMonth, &sDay);
+    sscanf(endDate, "%d-%d-%d", &eYear, &eMonth, &eDay);
 
-    int parkingYear, parkingMonth, parkingDay;
-    int departureYear, departureMonth, departureDay;
+    int durationDays = (eYear - sYear) * 365 + (eMonth - sMonth) * 30 + (eDay - sDay);
+    int sHour = convertTo24Hour(startTime);
+    int eHour = convertTo24Hour(endTime);
+    int durationHours = durationDays * 24 + (eHour - sHour);
 
-    sscanf(vehicle.parkingDate, "%d-%d-%d", &parkingYear, &parkingMonth, &parkingDay);
-    sscanf(departureDate, "%d-%d-%d", &departureYear, &departureMonth, &departureDay);
+    if (durationHours < 1) durationHours = 1;
 
-    int parkingDuration = (departureYear - parkingYear) * 365 + (departureMonth - parkingMonth) * 30 + (departureDay - parkingDay);
-
-    float totalCharge = (vehicle.type == 2) ? RATE_2_WHEELER * parkingDuration : RATE_4_WHEELER * parkingDuration;
-
-    printf("\n****************************************************\n");
-    printf("                    Parking Invoice\n");
-    printf("****************************************************\n");
-    printf("Vehicle: %s\n", vehicle.licensePlate);
-    printf("Owner's Name: %s\n", vehicle.ownerName);
-    printf("Type: %s\n", (vehicle.type == 2) ? "2-wheeler" : "4-wheeler");
-    printf("Parking Date: %s\n", vehicle.parkingDate);
-    printf("Departure Date: %s\n", departureDate);
-    printf("Parking Duration (in days): %d\n", parkingDuration);
-    printf("Rate per day: Rs%.2f\n", (vehicle.type == 2) ? RATE_2_WHEELER : RATE_4_WHEELER);
-    printf("Total charge: Rs%.2f\n", totalCharge);
-    printf("****************************************************\n");
+    float rate = (type == 2) ? 50.0 : 200.0;
+    return durationHours * rate;
 }
